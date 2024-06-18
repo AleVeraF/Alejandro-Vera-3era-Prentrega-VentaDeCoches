49e31e2db5801b0fe594efbb05bde96ba13cc39e
Alejandro Vera <alevera2709sv@gmail.com>
2024-06-18 23:08:58 +0200

    Agrego libreria sweetaler al proyecto

--- js/index.js
+++ js/index.js
@@ -3,6 +3,15 @@ const cards = document.getElementsByClassName("cards");
 const conteinerCoches = document.querySelector('#conteinerCoches')
 const carrito = document.querySelector('.carrito')
 
+function mensajeOk (titulo, mensaje, icono) { Swal.fire({
+    title: titulo,
+    text: mensaje,
+    icon: icono,
+    timer: 3500,
+    showConfirmButton: false,
+  });
+}
+
 function cardHtml(coches) {
     return `<div class="cards">
                     <h3 id="marca">${coches.modelo}</h3>
@@ -36,12 +45,13 @@ function ActivarClick() {
         botonAgregar.forEach((boton) => {
             boton.addEventListener("click", () => {
                 if (CompraCarrito.length > 0) {
-                    alert("Ya tienes un coche agregado al carrito");
+                    mensajeOk ("Solo puedes agregar un coche al carrito", "Ya tienes un coche en el carrito", "error");
                 } else {
                     const cocheSeleccionado = coches.find((coche) => coche.codigo == boton.id);
                     if (cocheSeleccionado) {
                         CompraCarrito.push(cocheSeleccionado);
                         console.table(CompraCarrito);
+                        mensajeOk ("¡Coche agregado al carrito!","!Puedes ir al carrito para finalizar la compra!", "success")
 
                         
                         localStorage.setItem('compraCarrito', JSON.stringify(CompraCarrito));
@@ -57,7 +67,7 @@ carrito.addEventListener("click", () => {
     if (CompraCarrito.length > 0) {
         location.href = "page/carrito.html";
     } else {
-        alert("Debes cargar un coche al carrito");
+        mensajeOk ("No has seleccionado ningún coche", "Debes agregar un coche al carrito", "warning");
     }
 });
 
