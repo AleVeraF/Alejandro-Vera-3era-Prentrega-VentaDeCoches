1cb789a24cc81a57dafc84262dc3dbc282bcffeb
Alejandro Vera <alevera2709sv@gmail.com>
2024-06-21 15:02:39 +0200

    se aplica sweetalert en todo el proyecto, y promesas

--- js/carrito.js
+++ js/carrito.js
@@ -5,11 +5,21 @@ const btnUltimaFinanciacion = document.querySelector("button#recuperar")
 const importe = document.querySelector("p.importe")
 const comprar = document.querySelector("button#comprar")
 const quitar = document.querySelector("button#quitar")
-
+const cargando = "<img src='../image/cargando.gif'>"
 const iva = 1.21
 const interes = 1.18;
 
 const arrayHistorial = []
+
+function mensajeOk(titulo, mensaje, icono) {
+    Swal.fire({
+        title: titulo,
+        text: mensaje,
+        icon: icono,
+        timer: 4500,
+        showConfirmButton: false,
+    });
+}
 
 function cargarFinanciarCoches() {
 
@@ -40,10 +50,16 @@ btnFinanciar.addEventListener('click', () => {
     const mesesSeleccionados = seleccionaMeses.value
     const importeCoche = importeFinanciado(modeloSeleccionado)
     if (importeCoche !== null) {
-        const pagoMensual = (parseInt(importeCoche)) / (parseInt(mesesSeleccionados)) * iva * interes
-        importe.textContent = `El importe a pagar mensual es: ${pagoMensual.toFixed(2)} €`
-        arrayHistorial.push({ modelo: modeloSeleccionado, meses: mesesSeleccionados, importe: importeCoche })
-        guardarFinanciacion()
+        btnFinanciar.innerHTML = cargando
+        setTimeout(() => {
+            const pagoMensual = (parseInt(importeCoche)) / (parseInt(mesesSeleccionados)) * iva * interes
+            importe.textContent = `El importe a pagar mensual es: ${pagoMensual.toFixed(2)} €`
+            arrayHistorial.push({ modelo: modeloSeleccionado, meses: mesesSeleccionados, importe: importeCoche })
+            guardarFinanciacion()
+            btnFinanciar.textContent = "Financiar"
+
+        }, 5000)
+
     } else {
         alert("No has seleccionado ningún coche")
     }
@@ -52,7 +68,7 @@ btnFinanciar.addEventListener('click', () => {
 btnUltimaFinanciacion.addEventListener("click", () => {
     const ultimaFinanciacion = recuperarUltimaFinanciacion()
     if (ultimaFinanciacion !== null) {
-        alert("Su última financiación fue el: " + ultimaFinanciacion.fecha + ultimaFinanciacion.valorCoche)
+        mensajeOk("Su última financiación fue el: " + ultimaFinanciacion.fecha + ultimaFinanciacion.valorCoche)
     } else {
         alert("No has solicitando ninguna financiación previamente.")
     }
@@ -84,8 +100,33 @@ function mostrarCarrito() {
 mostrarCarrito();
 
 comprar.addEventListener("click", () => {
-    const mensajeComprar = document.querySelector("div#mensajeComprar")
+
+    Swal.fire({
+        title: "Finalizar compra",
+        text: "El método de pago del coche será al contado ¿Es correcto?",
+        icon: "question",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+        confirmButtonText: "Confirmar",
+
+
+    })
+        .then((result) => {
+            if (result.isConfirmed) {
+                const mensajeComprar = document.querySelector("div#mensajeComprar")
     mensajeComprar.classList.add("compraRealizada")
+    carritoContenido.innerHTML = "No hay coches en el carrito"
     localStorage.removeItem("compraCarrito")
+    mensajeOk("¡Los datos de la compra han sido confirmados!", "Puedes ir a la pasarela de pago al realizar el pago correspondiente", "success")
+
+            }
+        })
+    
+})
+
+quitar.addEventListener ("click", () => {
+
+    carritoContenido.innerHTML = "No hay coches en el carrito"
+    mensajeOk("¡Coche eliminado correctamente!","", "success")
 
 })
